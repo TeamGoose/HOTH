e3266a9dc7c2ba5035cd0b74cb924d9261e40932
Johnny Urosevic <TheMagicalCake@gmail.com>
2017-11-12 12:02:57 -0800

    oop
    https://docs.djangoproject.com/en/1.11/intro/tutorial02/

--- Swipes/Sell/models.py
+++ Swipes/Sell/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class SellEntry(models.Model):
+    DINING_HALL_CHOICES = (
+        ('FST', 'Feast'),
+        ('COV', 'Covel'),
+        ('DNVE', 'De Neve'),
+        ('BPLT', 'Bruin Plate'),
+        ('1919', 'Cafe 1919'),
+        ('BCAF', 'Bruin Cafe'),
+        ('STDY', 'The Study'),
+        ('REND', 'Rendevous'),
+    )
+    dining_hall = models.CharField(
+        max_length=4,
+        choices=DINING_HALL_CHOICES,
+        default='FST',
+    )
+    comment = models.CharField(max_length=200)
