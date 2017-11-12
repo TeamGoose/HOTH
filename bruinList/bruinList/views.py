bbf1a49cab04a7a853ebb5a2d1e514c423bec488
Wilson Jusuf <wilsonjusuf1998@gmail.com>
2017-11-12 15:58:22 -0800

    views

--- bruinList/bruinList/views.py
+++ bruinList/bruinList/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from ...sell.models import sellEntry
 # Create your views here.
+import sys
+sys.path.insert(0, "../sell")
+try:
+    from .models import sellEntry
+except ImportError:
+    print('No Import')
 def index (request):
-	return HttpResponse('<h1> Bruin List </h1>')
+	   db =1
+        #return HttpResponse('<h1> Bruin List </h1>')
