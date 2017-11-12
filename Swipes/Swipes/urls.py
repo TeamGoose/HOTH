04b91b1925c4de093e1608504458c876b3b53dbf
varun1729 <varun.narayanan1729@gmail.com>
2017-11-12 11:49:43 -0800

    Homepage include

--- Swipes/Swipes/urls.py
+++ Swipes/Swipes/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^Sell/', include('Sell.urls')),
-    #url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^',include ('homePage.urls'))
 ]
--- Swipes/homePage/views.py
+++ Swipes/homePage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1> Swipes </h1>")
+    return HttpResponse("<h1> Swipe Me In! or any other working title </h1>")
