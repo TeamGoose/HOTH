9758ac0aa23ce6422d0394c70e0cae32730f3517
Wilson Jusuf <wilsonjusuf1998@gmail.com>
2017-11-12 17:41:05 -0800

    moved templates back to website directory

--- website/bruinList/settings.py
+++ website/bruinList/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bruinList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"bruinList/templates")],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
