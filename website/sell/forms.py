8629291fc4db977502afec2b2b247d11cd69d2fe
Johnny Urosevic <TheMagicalCake@gmail.com>
2017-11-12 19:44:21 -0800

    weee

---
+++ website/sell/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class SellForm(forms.Form):
+	itemName = form.CharField(max_length = 50)
+	sellerName = form.CharField(max_length = 50)
+	description = form.CharField(max_length = 100)
+	price = form.DecimalField(max_length = 10, decimal_places = 2)
+	emailSeller = form.CharField(max_length = 100)
+	sellerNumber = form.CharField(max_length = 10)
--- website/sell/views.py
+++ website/sell/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index (request):
 	return HttpResponse('<h1> Sell </h1>')
+
+def getSale (request):
+	if request.method == 'POST':
+		form = SellForm(request.post)
+		if form.is_valid():
+			#process the data
+			itemName = request.POST.get('itemName')
+			sellerName = request.POST.get('sellerName')
+			description = request.POST.get('description')
+			price = request.POST.get('price')
+			emailSeller = request.POST.get('emailSeller')
+			sellerNumber = request.POST.get('sellerNumber')
+			sell_form = sellForm(itemName = itemName, sellerName = sellerName, description = description, price = price, emailSeller = emailSeller, sellerNumber = sellerNumber)
+			sell_form.save()
+			return HttpResponseRedirect('/')
+
+		else:
+			form = SellForm()
+
+		return render(request, 'sell.html', {'form': form})
